5d1e837bd06b9d644a9174addb0ad2e29bd2608c
Robert Lynch <robertflynch@gmail.com>
2018-12-13 18:09:07 +0200

    added dialect matrix and distance matrix

--- get_data.R
+++ get_data.R
@@ -8,14 +8,33 @@
 # sheet names are:
 #  munic_name_matching, ling_dist, geog_dist
 
-library("openxlsx")
-g <- read.xlsx("geog_ling_dists.xlsx", sheet = "geog_dist", startRow = 1, colNames = TRUE)
-#I also found that I've turned the matrices to vectors with 'melt' function in 'reshape' package
-#I had used it like this: 
+# the main problem here is that the sheets need to be read in amatrices and must inlcude the 1st column as row numbers
+#key lines header = TRUE and row.names = (column) 1
+geogdist<-read.table("geog_dist.csv", sep= ",",header=TRUE, row.names=1) #read in the language data
+geogdist<-as.dist(geogdist) # turn the data into distances 
+geogmatx<-as.matrix(geogdist) #makes a distance matrix
 library("reshape")
-langdist1 <- melt(g)[melt(upper.tri(g))$value,]
-names(langdist1) <- c("c1", "c2", "lang_dist") 
+geogdist1 <- melt(geogmatx)[melt(upper.tri(geogmatx))$value,]
+names(geogdist1) <- c("m1", "m2", "geog_dist") 
 
-# change git repository
-# 
-# ## this is bullshit
+# get lingusitic distances
+lingdist<-read.table("ling_dist.csv", sep= ",",header=TRUE, row.names=1) #read in the language data
+lingdist<-as.dist(lingdist) # turn the data into distances 
+lingmatx<-as.matrix(lingdist) #makes a distance matrix
+library("reshape")
+lingdist1 <- melt(lingmatx)[melt(upper.tri(lingmatx))$value,]
+names(lingdist1) <- c("m1", "m2", "ling_dist") 
+
+# match the names to the ids
+muni_names <- read.table("name_matches.csv", sep=",",header=TRUE)
+library(dplyr)
+# left join ids to ling and geog flattened matrices and add actual names
+data <- geogdist1 %>% left_join(lingdist1, by=c("m1"="m1","m2"="m2"))
+data <- data %>% left_join(muni_names, by=c("m1"="id")) 
+
+data <- plyr::rename(data, c("municipality"="municipality_1"))
+
+data <- data %>% left_join(muni_names, by=c("m2"="id")) 
+data <- plyr::rename(data, c("municipality"="municipality_2"))
+
+saveRDS(data, "distance_matrices")
